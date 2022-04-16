ea26386a3be422f9964ddffb8d584dc3ba6ced34
Fozimat <fozimata@gmail.com>
2022-04-16 19:32:31 +0700

    Remake register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
@@ -82,4 +82,96 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection --}}
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Register</title>
+    <link rel="stylesheet" href="{{ asset('assets/vendors/mdi/css/materialdesignicons.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/flag-icon-css/css/flag-icon.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/css/vendor.bundle.base.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/font-awesome/css/font-awesome.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}" />
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" />
+</head>
+
+<body>
+    <div class="content-wrapper vh-100">
+        <div class="row d-flex align-items-center justify-content-center h-100">
+            <div class="col-md-4 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <img style="width: 300px;
+                    max-width: 500px;
+                    height: 100px;
+                    margin-left: 25px;" src="{{ asset('assets/images/logo.svg') }}" alt="logo" />
+
+                        <form class="forms-sample mb-5" method="POST" action="{{ route('register') }}">
+                            @csrf
+                            <div class="form-group">
+                                <label for="nama">Nama</label>
+                                <input required type="nama" name="nama"
+                                    class="form-control @error('nama') is-invalid @enderror" id="nama"
+                                    placeholder="Nama" autocomplete="nama" autofocus value="{{ old('nama') }}">
+                                @error('nama')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input required type="email" name="email"
+                                    class="form-control @error('email') is-invalid @enderror" id="email"
+                                    placeholder="Email" autocomplete="email" value="{{ old('email') }}">
+                                @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" name="password"
+                                    class="form-control @error('password') is-invalid @enderror" id="password"
+                                    placeholder="Password" required autocomplete="new-password">
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                            <div class="form-group">
+                                <label for="password-confirm">Password</label>
+                                <input type="password" name="password_confirmation"
+                                    class="form-control @error('password') is-invalid @enderror" id="password-confirm"
+                                    placeholder="Password" required autocomplete="new-password">
+                                @error('password')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                                @enderror
+                            </div>
+                            <button type="submit" class="btn btn-primary mr-2"> Register </button>
+                            @if (Route::has('password.request'))
+                            <a class="btn btn-link text-decoration-none" href="{{ route('login') }}">
+                                <div class="text-muted">Sudah punya akun ? Login</div>
+                            </a>
+                            @endif
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="{{ asset('assets/vendors/js/vendor.bundle.base.js') }}"></script>
+    <script src="{{ asset('assets/js/off-canvas.js') }}"></script>
+    <script src="{{ asset('assets/js/hoverable-collapse.js') }}"></script>
+    <script src="{{ asset('assets/js/misc.js') }}"></script>
+</body>
+
+</html>
